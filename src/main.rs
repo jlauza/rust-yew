61031d2ab026e3b94aef88a1231f3498249d9f7b
jlauza <lauzajayson@gmail.com>
2025-06-14 00:16:59 +0800

    simple counter example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-fn main() {
-    let num1 = 5;
-    let num2 = 10;
+use yew::prelude::*;
 
-    println!("5 === 10 is {}", num1 == num2);
-    println!("5 != 10 is {}", num1 != num2);
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+
+    html! {
+        <>
+            <h1>{ "Counter Example" }</h1>
+            <button {onclick}>{ "Add +1" }</button>
+            <p>{format!("Counter value: {}", *counter)}</p>
+        </>
+    }    
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
 }
